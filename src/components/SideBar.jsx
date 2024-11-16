adb3df9bb8a626038010d7d28e9d37055f9919d4
Shivam <shivamtiwarix88@gmail.com>
2024-11-16 16:54:21 +0530

    update

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -42,11 +42,11 @@ export const SideBar = () => {
         {menuItems.map((item, index) => (
           <MenuItem key={index} icon={item.icon} label={item.label} />
         ))}
-        <div className="flex items-center ml-4 mr-4 p-2 rounded-3xl hover:bg-blue-100 hover:text-black">
+        <div className="flex items-center ml-2 mr-2 p-2 rounded-3xl hover:bg-blue-100 hover:text-black">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10 pr-2">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a2.25 2.25 0 0 0-2.25-2.25H15a3 3 0 1 1-6 0H5.25A2.25 2.25 0 0 0 3 12m18 0v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 9m18 0V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v3" />
             </svg>
-            <div className="font-mono line-clamp-2 text-sm">
+            <div className="font-mono line-clamp-2 w-9/12 text-sm">
                 Current Balance: $7783434342.50
             </div>
         </div>
